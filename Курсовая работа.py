9c590a8654741e664a0a71ed9b9a51d40cae03e6
Lyota <korystin.aleksei@yandex.ru>
2023-02-05 00:44:48 +0300

    update

--- Курсовая работа.py
+++ Курсовая работа.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import time
+import datetime
 import json
 from tqdm import tqdm
 from pprint import pprint
@@ -17,10 +17,15 @@ def find_max_resolution(glossary):
     max_resolution = 0
     for i in range(len(glossary)):
         result = glossary[i].get('width') * glossary[i].get('height')
-        if max_resolution > result:
-            result = max_resolution
+        if result > max_resolution:
+            max_resolution = result
             max_photo = i
-        return glossary[max_photo].get('url'), glossary[max_photo].get('type')
+    return glossary[max_photo].get('url'), glossary[max_photo].get('type')
+
+def time_convert(unix):
+    time_unix = datetime.datetime.fromtimestamp(unix)
+    normal_time = time_unix.strftime('time: %H:%M:%S date: %d.%m.%Y')
+    return normal_time
 
 class vkontakte:
     def __init__(self, token, version='5.131'):
@@ -28,9 +33,8 @@ class vkontakte:
         self.id = token[1]
         self.version = version
         self.initial_params = {'access_token': self.token, 'v': self.version}
-        self.json = self.get_information_about_the_photo()
-        # self.json, self.export_dict = self._sort_info()
-
+        self.json, self.download_information = self.photo_data_collection()
+        
     def get_information_about_the_photo(self):
         url = 'https://api.vk.com/method/photos.get'
         params = {'owner_id': self.id,
@@ -38,39 +42,36 @@ class vkontakte:
                   'photo_sizes': 1,
                   'extended': 1}
         response = requests.get(url, params={**self.initial_params, **params}).json()
-        return response['response']['count'] #response['response']['items']
+        return response['response']['count'], response['response']['items']
 
+    def photo_data_collection(self):
+        data_collection = {}
+        json_file = []
+        number_photos, photo_elements = self.get_information_about_the_photo()
+        for i in range(number_photos):
+            photo_address, photo_size = find_max_resolution(photo_elements[i]['sizes'])
+            
+            if _ not in json_file['file_name']:
+                json_file.append({
+                "file_name": f'{photo_elements[i]["likes"]["count"]}.jpeg',
+                "size": photo_size
+                })
+        # if name not in json_file[file_name]:
+        #     data_collection[int(i) + 1] = {
+        #         "file_name": f'{photo_elements[i]["likes"]["count"]}.jpeg',
+        #         "url": photo_address
+        #         }
+        # data_collection[photo_elements[i]['likes']['count']] = {'url':photo_address}
+        #     photo_address = find_max_resolution(photo_elements[i]['sizes'])
+        #     likes = photo_elements[i]['likes']['count']
+        #     photo_date = time_convert(photo_elements[i]['date'])
+        #     new_value = data_collection.get(likes)
+        #     new_value.append({'add_name': photo_date,
+        #                       'url_picture': photo_address})
+        #                       #'size': picture_size})
+        #     data_collection[likes] = new_value
+        return json_file, data_collection
     
-
-# id = "13162384"
-# token = ''
-# params = {'access_token': token,
-#           'owner_id': id,
-#           'album_id': 'profile',
-#           'photo_sizes': 1,
-#           'extended': 1,
-#           'v': 5.131
-#           }
-# url = 'https://api.vk.com/method/photos.get'
-# response = requests.get(url=url, params=params).json()
-# pprint(response)
-# print(response['response']['count'], response['response']['items'])
-
 if __name__ == '__main__':
     res = vkontakte(open_a_token('token.ini'))
     pprint(res.json)
-
-# self.json = self.get_information_about_the_photo()
-
-# #     def get_information_about_the_photo(self):
-# #         url = 'https://api.vk.com/method/photos.get'
-# #         params = {'owner_id': self.id,
-# #                   'album_id': 'profile',
-# #                   'photo_sizes': 1,
-# #                   'extended': 1}
-# #         response = requests.get(url, params={'access_token': self.oken,
-# #                                              'owner_id': selfid,
-# #                                              'album_id': 'profile',
-# #                                              'photo_sizes': 1,
-# #                                              'extended': 1,
-# #                                              'v': 5.131}).json()
